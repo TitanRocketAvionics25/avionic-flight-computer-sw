28c0a5cdd664aa97c698ef1b4781524b0984ea92
dylanR33 <ruiz.dylaneduardo@gmail.com>
2025-03-06 00:05:28 -0800

    Fixed naming convention for i2c config.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 sources = src/drivers/bmi088 src/drivers/bmp581
 
-hw_sources = src/ src/startup src/SystemClock src/I2CInit
+hw_sources = src/ src/startup src/SystemClock src/i2cconf
 
 WFLAGS = -Wall
 
--- src/i2cconf/i2cconf.c
+++ src/i2cconf/i2cconf.c
@@ -1,4 +1,4 @@
-#include "I2Cconf.h"
+#include "i2cconf.h"
 
 #include <stdio.h>
 
@@ -7,7 +7,7 @@
 
 static I2C_HandleTypeDef i2ch = { 0 };
 
-void I2Cconf_Config()
+void i2cconf_config()
 {
     i2ch.Instance             = I2C1;
     i2ch.Init.ClockSpeed      = 10240;
@@ -22,13 +22,13 @@ void I2Cconf_Config()
     HAL_I2C_Init( &i2ch );
 }
 
-void I2Cconf_Read( uint8_t* dataOut, uint16_t dataSize, uint8_t* rxBuff, uint16_t rxSize )
+void i2cconf_read( uint8_t* dataOut, uint16_t dataSize, uint8_t* rxBuff, uint16_t rxSize )
 {
     HAL_I2C_Mem_Read( &i2ch, dataOut[ dataSize - 2], dataOut[ dataSize - 1 ], 
                       I2C_MEMADD_SIZE_8BIT, rxBuff, rxSize, HAL_MAX_DELAY );
 }
 
-void I2Cconf_Write( uint8_t* dataOut, uint16_t dataSize )
+void i2cconf_write( uint8_t* dataOut, uint16_t dataSize )
 {
     HAL_I2C_Master_Transmit( &i2ch, dataOut[ 0 ], dataOut + 1, dataSize - 1, HAL_MAX_DELAY );
 }
