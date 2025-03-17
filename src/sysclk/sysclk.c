cfa3ad522069bf1d674aee9a94fb097ff893640e
dylanR33 <ruiz.dylaneduardo@gmail.com>
2025-03-17 12:47:45 -0700

    Changed clock configuration to stay within limits of each bus.

--- src/sysclk/sysclk.c
+++ src/sysclk/sysclk.c
@@ -20,12 +20,12 @@ void sysclk_config()
   /* Enable HSI Oscillator and activate PLL with HSI as source */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = 0x10;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 16;
-  RCC_OscInitStruct.PLL.PLLN = 400;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;  // 16 MHz
+  RCC_OscInitStruct.PLL.PLLM = 8;                       // fpll_in  = 2 MHz
+  RCC_OscInitStruct.PLL.PLLN = 120;                     // fvco_out = 240 MHz
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;           // fpll_out = 120 MHz
   RCC_OscInitStruct.PLL.PLLQ = 7;
   HAL_RCC_OscConfig(&RCC_OscInitStruct);
   
@@ -33,9 +33,9 @@ void sysclk_config()
      clocks dividers */
   RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;  
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;  
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;    // 120 MHz
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;     // 30  MHz
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;     // 60  MHz
   HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3);
   
 }
