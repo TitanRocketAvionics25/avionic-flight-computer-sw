13bd3143594d074a976f53c8515acd28660f0f4f
dhnpx <122553352+dhnpx@users.noreply.github.com>
2025-03-18 01:13:27 -0700

    Wrote erase and write

--- src/drivers/w25q64jvssiq/w25q64jvssiq.c
+++ src/drivers/w25q64jvssiq/w25q64jvssiq.c
@@ -1,36 +1,160 @@
 #include "w25q64jvssiq.h"
 #include "SPI1.h"
+#include "delay.h"
+
+uint16_t number_of_sectors;
 
 /* Software reset requires two instructions Enable Reset (66h) and
 * Reset (99h) and take approximately 30µS. No instructions 
 * are accepted during */
 
 void W25Q64JV_reset(void) {
-    uint8_t t_data[2];
-    t_data[0] = 0x66; // Enable Reset (66h)
-    t_data[1] = 0x99; // Reset (99h)
+    uint8_t data_tx[2];
+    data_tx[0] = 0x66; // Enable Reset (66h)
+    data_tx[1] = 0x99; // Reset (99h)
 
     W25Q64JV_cs_low();
-    SPI1_transmit(t_data, 2);
+    SPI1_transmit(data_tx, 2);
     W25Q64JV_cs_high();
 }
 
 /* Reads JEDEC ID */
 
 uint32_t W25Q64JV_readID(void) {
-    uint8_t t_data = 0x9f; // Instruction for JEDEC ID. 
-    uint8_t r_data[3];
+    uint8_t data_tx = 0x9f; // Instruction for JEDEC ID. 
+    uint8_t data_read[3];
 
     W25Q64JV_cs_low();
-    SPI1_transmit(&t_data, 1);
-    SPI1_receive(r_data, 3);
+    SPI1_transmit(&data_tx, 1);
+    SPI1_receive(data_read, 3);
     W25Q64JV_cs_high();
 
 
     /* Manufacturer ID : Memory Type ID : Capacity ID */
-    return ((r_data[0] << 16) | (r_data[1] << 8)| r_data[2]);
+    return ((data_read[0] << 16) | (data_read[1] << 8)| data_read[2]);
 }
 
+void W25Q64JV_number_of_sector(uint16_t sector_num) {
+    number_of_sectors = sector_num;
+}
+
+void W25Q64JV_read(uint32_t sector, uint8_t offset, uint8_t *data_read, uint32_t size) {
+    uint8_t data_tx[4];
+    uint32_t mem_addr = (sector * 256) + offset;
+
+    W25Q64JV_cs_low();
+
+    data_tx[0] = 0x03; // Enable read
+    data_tx[1] = (mem_addr >> 16) & 0xFF; // MSB of mem_addr
+    data_tx[2] = (mem_addr >> 8) & 0xFF;
+    data_tx[3] = (mem_addr) & 0xFF; // LSB of mem_addr
+
+    SPI1_transmit(data_tx, 4);
+    SPI1_receive(data_read, size);
+    W25Q64JV_cs_high();
+}
+
+static void write_enable(void) {
+    uint8_t data_tx = 0x06; // Enable write
+    W25Q64JV_cs_low();
+    SPI1_transmit(&data_tx, 1);
+    W25Q64JV_cs_high();
+    delay(5); //5ms delay
+}
+
+static void write_disable(void) {
+    uint8_t data_tx = 0x04;
+    W25Q64JV_cs_low();
+    SPI1_transmit(&data_tx, 1);
+    W25Q64JV_cs_high();
+    delay(5);
+}
+
+void W25Q64JV_erase_sector(uint16_t sector) {
+    uint8_t data_tx[4]; 
+    uint32_t mem_addr = sector * 16 * 256;
+
+    write_enable();
+
+    data_tx[0] = 0x20; //Erase Sector
+    data_tx[1] = (mem_addr >> 16) & 0xFF; // MSB of mem_addr
+    data_tx[2] = (mem_addr >> 8) & 0xFF; 
+    data_tx[3] = (mem_addr) & 0xFF; // LSB of mem_addr
+    
+    W25Q64JV_cs_low();
+    SPI1_transmit(data_tx, 4);
+    W25Q64JV_cs_high();
+
+    delay(500); // 400ms delay max for sector erase. Just to be sure 500ms.
+
+    write_disable();
+}
+
+static uint32_t bytes_to_write (uint32_t size, uint16_t offset) {
+    if ((size + offset) < 256 ) {
+        return size;
+    } else {
+        return (256 - offset);
+    }
+}
+
+void W25Q64JV_write(uint32_t page, uint16_t offset, uint8_t *data, uint32_t size) {
+    uint8_t data_tx[260];
+    uint32_t page_starting = page;
+    uint32_t page_ending = page_starting + ((size + offset - 1) / 256);
+    uint32_t page_nums = page_ending - page_starting;
+    
+    uint16_t sector_starting = page_starting / 16;
+    uint16_t sector_ending = page_ending / 16;
+    uint16_t sector_nums = sector_ending - sector_starting + 1;
+
+    for (uint16_t i = 0; i < sector_nums; i++) {
+        W25Q64JV_erase_sector(sector_starting++);
+    }
+
+    uint32_t idx_data = 0;
+    for (uint32_t i = 0; i < page_nums; i++) {
+        uint32_t mem_addr = (page_starting * 256) + offset;
+        uint16_t bytes_remaining = bytes_to_write(size, offset);
+        
+        uint32_t idx_data_tx = 0;
+        write_enable();
+
+        data_tx[0] = 0x02; //page program
+        data_tx[1] = (mem_addr >> 16) & 0xFF;
+        data_tx[2] = (mem_addr >> 8) & 0xFF; 
+        data_tx[3] = (mem_addr) & 0xFF;
+        
+        idx_data_tx = 5;
+
+        uint16_t bytes_to_send = bytes_remaining + idx_data_tx;
+
+        for (uint16_t j = 0; j < bytes_remaining; j++) {
+            data_tx[idx_data_tx++] = data[j + idx_data];
+        }
+
+        W25Q64JV_cs_low();
+        SPI1_transmit(data_tx, bytes_to_send);
+        W25Q64JV_cs_high();
+
+        page_starting++;
+        offset = 0;
+        size = size - bytes_remaining;
+        idx_data = idx_data + bytes_remaining;
+
+        delay(5); 
+        write_disable();
+    }
+
+}
+
+
+/*
+ * 128 blocks (64KB)
+ * 1 block = 16 sectors (4KB)
+ * 1 sector = 16 pages (256 bytes)
+ *
+ */
 
 /*
  * init bolierplate
