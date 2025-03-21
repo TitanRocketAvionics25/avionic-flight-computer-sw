d3b0b2d6e93c142a515d9c2b2e8355f4eff9cf95
dylanR33 <ruiz.dylaneduardo@gmail.com>
2025-03-21 16:43:46 -0700

    Added functionality for basic reading of temperature and pressure data to bmp driver.

--- src/drivers/bmp581/bmp581.c
+++ src/drivers/bmp581/bmp581.c
@@ -3,25 +3,132 @@
 
 #include <stddef.h>
 
+// Buffer sizes
+#define READ_BUFF_SIZE  2
+#define WRITE_BUFF_SIZE 3
+#define TEMP_BUFF_SIZE  3
+#define PRESS_BUFF_SIZE 3
+
+// Indexes for I2C transmission buffers
+#define SLAVE_ADDR      0
+#define REG_ADDR        1
+#define DATA            2
+
+// Indexes for temperature and pressure data
+#define XLSB            0
+#define LSB             1
+#define MSB             2
+
+// TTP stand for "to the power"
+#define TWO_TTP_16      65536.f
+#define TWO_TTP_6       64.f
+
 
 void bmp581_read(  uint8_t startAddr, uint8_t* rxBuff, uint16_t rxSize, bmp581_t* bmp )
 {
-    uint8_t dataBuff[ 2 ] = { 0 };
+    uint8_t dataBuff[ READ_BUFF_SIZE ] = { 0 };
 
-    dataBuff[ 0 ] = BMP581_SLAVE_ADDRESS << 1;
-    dataBuff[ 1 ] = startAddr;
+    dataBuff[ SLAVE_ADDR ] = BMP581_SLAVE_ADDRESS << 1;
+    dataBuff[ REG_ADDR   ] = startAddr;
 
     bmp->i2c_read( dataBuff, sizeof( dataBuff ), rxBuff, rxSize );
 }
 
+
+uint8_t bmp581_read_byte( uint8_t addr, bmp581_t* bmp )
+{
+    uint8_t reading;
+    bmp581_read( addr, &reading, sizeof( reading ), bmp );
+    return reading;
+}
+
+
 void bmp581_write( uint8_t addr, uint8_t data, bmp581_t* bmp )
 {
-    uint8_t dataBuff[ 3 ] = { 0 };
+    uint8_t dataBuff[ WRITE_BUFF_SIZE ] = { 0 };
 
-    dataBuff[ 0 ] = BMP581_SLAVE_ADDRESS << 1;
-    dataBuff[ 1 ] = addr;
-    dataBuff[ 2 ] = data;
+    dataBuff[ SLAVE_ADDR ] = BMP581_SLAVE_ADDRESS << 1;
+    dataBuff[ REG_ADDR   ] = addr;
+    dataBuff[ DATA       ] = data;
 
     bmp->i2c_write( dataBuff, sizeof( dataBuff ) );
 }
 
+
+void bmp581_disable_deep_standby( bmp581_t* bmp )
+{
+    uint8_t cmd = bmp581_read_byte( BMP581_ODR_CONFIG, bmp ) | 
+                                  ( 1 << BMP581_ODR_CONFIG_DEEP_DIS_Pos );
+    bmp581_write( BMP581_ODR_CONFIG, cmd, bmp );
+}
+
+
+void bmp581_standby_mode( bmp581_t* bmp )
+{
+    uint8_t cmd = bmp581_read_byte( BMP581_ODR_CONFIG, bmp ) & 
+                                 ~( BMP581_ODR_CONFIG_PWR_MODE_Msk );
+    cmd |= ( BMP581_ODR_CONFIG_PWR_MODE_STNBY << BMP581_ODR_CONFIG_PWR_MODE_Pos );
+
+    bmp581_write( BMP581_ODR_CONFIG, cmd, bmp );
+}
+
+
+void bmp581_normal_mode( bmp581_t* bmp )
+{
+    bmp581_standby_mode( bmp );
+    // After the above the power mode bits are zero'ed so no need to rezero them
+    uint8_t cmd = bmp581_read_byte( BMP581_ODR_CONFIG, bmp ) | 
+                                  ( BMP581_ODR_CONFIG_PWR_MODE_NORM << 
+                                    BMP581_ODR_CONFIG_PWR_MODE_Pos );
+
+    bmp581_write( BMP581_ODR_CONFIG, cmd, bmp );
+}
+
+
+void bmp581_non_stop_mode( bmp581_t* bmp )
+{
+    bmp581_standby_mode( bmp );
+    // After the above the power mode bits are zero'ed so no need to rezero them
+    uint8_t cmd = bmp581_read_byte( BMP581_ODR_CONFIG, bmp ) | 
+                                  ( BMP581_ODR_CONFIG_PWR_MODE_NON_STOP << 
+                                    BMP581_ODR_CONFIG_PWR_MODE_Pos );
+
+    bmp581_write( BMP581_ODR_CONFIG, cmd, bmp );
+}
+
+
+void bmp581_enable_press( bmp581_t* bmp )
+{
+    uint8_t cmd = bmp581_read_byte( BMP581_OSR_CONFIG, bmp ) | 
+                                  ( 1 << BMP581_OSR_CONFIG_PRESS_EN_Pos );
+
+    bmp581_write( BMP581_OSR_CONFIG, cmd, bmp );
+}
+
+
+int32_t bmp581_get_raw_temp( bmp581_t* bmp )
+{
+    uint8_t rx[ TEMP_BUFF_SIZE ] = { 0 };
+    bmp581_read( BMP581_TEMP_DATA_XLSB, rx, sizeof( rx ), bmp );
+    return ( int32_t )( ( rx[ MSB ] << 16 ) | ( rx[ LSB ] << 8 ) | rx[ XLSB ] );
+}
+
+
+uint32_t bmp581_get_raw_press( bmp581_t* bmp )
+{
+    uint8_t rx[ PRESS_BUFF_SIZE ] = { 0 };
+    bmp581_read( BMP581_PRESS_DATA_XLSB, rx, sizeof( rx ), bmp );
+    return ( uint32_t )( ( rx[ MSB ] << 16 ) | ( rx[ LSB ] << 8 ) | rx[ XLSB ] );
+}
+
+
+float bmp581_get_temp( bmp581_t* bmp )
+{
+    return bmp581_get_raw_temp( bmp ) / TWO_TTP_16;
+}
+
+
+float bmp581_get_press( bmp581_t* bmp )
+{
+    return bmp581_get_raw_press( bmp ) / TWO_TTP_6;
+}
--- src/drivers/bmp581/bmp581.h
+++ src/drivers/bmp581/bmp581.h
@@ -5,13 +5,6 @@
 #include <stdint.h>
 
 
-typedef enum
-{
-    BMP581_SUCCESS,
-    BMP581_FAIL
-} bmp581_status_t;
-
-
 // Defines i2c interaction callbacks
 //
 // i2c_write : write bytes over i2c
@@ -30,6 +23,9 @@ typedef struct
 } bmp581_t;
 
 
+// Functions with the parameter bmp581_t* bmp refer to the bmp581 instance to use
+
+
 // Read registers starting from an address
 //
 // NOTE: the bmp581 auto increments the address so that multiple sequential registers can be read
@@ -40,9 +36,15 @@ typedef struct
 // rxBuff    : buffer to hold read back bytes
 //
 // rxSize    : total bytes to read
-//
-// bmp       : bmp581 instance
 void bmp581_read( uint8_t startAddr, uint8_t* rxBuff, uint16_t rxSize, bmp581_t* bmp );
+
+
+// Read single byte within register address
+//
+// addr   : register address to read
+//
+// return : register data
+uint8_t bmp581_read_byte( uint8_t addr, bmp581_t* bmp );
 
 
 // Write to a single register
@@ -50,9 +52,51 @@ void bmp581_read( uint8_t startAddr, uint8_t* rxBuff, uint16_t rxSize, bmp581_t*
 // addr    : register address to write to
 //
 // data    : data to write into register
-//
-// bmp     : bmp581 instance
 void bmp581_write( uint8_t addr, uint8_t data, bmp581_t* bmp );
+
+
+// Disable the deep standby the chip goes into upon startup
+void bmp581_disable_deep_standby( bmp581_t* bmp );
+
+
+// Enter standby mode
+void bmp581_standby_mode( bmp581_t* bmp );
+
+
+// Enter normal mode
+void bmp581_normal_mode( bmp581_t* bmp );
+
+
+// Enter non-stop/continuous mode
+void bmp581_non_stop_mode( bmp581_t* bmp );
+
+
+// Enable pressure measurements (off at startup)
+void bmp581_enable_press( bmp581_t* bmp );
+
+
+// Get raw temperature reading
+//
+// return : raw temperature
+int32_t bmp581_get_raw_temp( bmp581_t* bmp );
+
+
+// Get raw pressure reading
+//
+// return : raw pressure
+uint32_t bmp581_get_raw_press( bmp581_t* bmp );
+
+
+// Get converted temperature reading in degrees celsius
+//
+// return : temperature [deg C]
+float bmp581_get_temp( bmp581_t* bmp );
+
+
+// Get converted pressure reading in pascals
+//
+// return pressure [Pa]
+float bmp581_get_press( bmp581_t* bmp );
 
 
 #endif
